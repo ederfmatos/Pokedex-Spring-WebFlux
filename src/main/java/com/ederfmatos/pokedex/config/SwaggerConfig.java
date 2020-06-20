d9e0effa700dd3f341046c63180774cbb0dcb7ef
ederfmatos <ederfmatos@gmail.com>
2020-06-20 12:39:29 -0300

    Alterado path de controller para Swagger

--- src/main/java/com/ederfmatos/pokedex/config/SwaggerConfig.java
+++ src/main/java/com/ederfmatos/pokedex/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(PathSelectors.any())
-                .apis(RequestHandlerSelectors.basePackage("com.ederfmatos.pokedex"))
+                .apis(RequestHandlerSelectors.basePackage("com.ederfmatos.pokedex.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .useDefaultResponseMessages(false)
