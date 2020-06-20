2aebd90f149b5cd7afcc068fd172dec894c0319a
ederfmatos <ederfmatos@gmail.com>
2020-06-20 11:37:13 -0300

    Teste CI&CD

--- src/main/java/com/ederfmatos/pokedex/model/Pokemon.java
+++ src/main/java/com/ederfmatos/pokedex/model/Pokemon.java
@@ -19,7 +19,7 @@ public class Pokemon {
     private double weight;
     private String image;
 
-    private boolean test = true;
+    private boolean test = false;
 
     public Pokemon() {
     }
