5d9ad31c0be68bfad78b5120f0f4c0e9ebe8afb1
ederfmatos <ederfmatos@gmail.com>
2020-07-04 16:41:31 -0300

    Adicionado cors

--- src/main/java/com/ederfmatos/pokedex/config/CorsConfig.java
+++ src/main/java/com/ederfmatos/pokedex/config/CorsConfig.java
@@ -1,4 +1,31 @@
 package com.ederfmatos.pokedex.config;
 
-public class CorsConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfigurationSource corsConfigurationSource = exchange -> null;
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setMaxAge(3600L);
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return new CorsWebFilter(source);
+    }
+
 }
